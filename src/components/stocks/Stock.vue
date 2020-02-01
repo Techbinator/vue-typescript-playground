c6185bfd65996c77e1f529640863f7d9c338f02e
techbinator <tudor@filipovici.com>
2020-02-01 20:55:23 +0100

    Implement basic functionality using local state

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,56 @@
 <template>
-  <h1>Stocks stock Component</h1>
+  <div class="col-sm-6 col-md-4 stock">
+    <div class="card">
+      <div class="card-body">
+        <h3 class="card-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+        <div>
+          <input placeholder="quantity" type="number" class="form-control" v-model="quantity" />
+        </div>
+        <div>
+          <button class="btn btn-success" :disabled="quantity <=0" @click="buyStock">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+<script lang="ts">
+import Vue from "vue";
+interface IStock {
+  id: number;
+  price: number;
+  name: string;
+}
+export default Vue.extend({
+  data() {
+    return {
+      quantity: "0"
+    };
+  },
+  props: {
+    stock: {
+      type: Object as () => IStock
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: parseInt(this.quantity)
+      };
+      this.quantity = "0";
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.stock {
+  margin-top: 10px;
+}
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Stocks stocks Component</h1>
+  <div class="row">
+    <stock :stock="stock" v-for="stock in stocks" :key="stock.id" />
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Stock from "./Stock.vue";
+
+export default Vue.extend({
+  components: {
+    stock: Stock
+  },
+  data() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: "BMW",
+          price: 110
+        },
+        {
+          id: 2,
+          name: "Google",
+          price: 200
+        },
+        {
+          id: 3,
+          name: "Apple",
+          price: 250
+        },
+        {
+          id: 4,
+          name: "Twitter",
+          price: 8
+        }
+      ]
+    };
+  }
+});
+</script>
