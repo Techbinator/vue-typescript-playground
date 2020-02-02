ef5b9dbc2128a9e70eddfbdc594462d82834cc45
techbinator <tudor@filipovici.com>
2020-02-02 11:16:42 +0100

    Add route change animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Header />
     <div>
       <div class="col-xs-12">
-        <router-view />
+        <transition name="slide" mode="out-in">
+          <router-view />
+        </transition>
       </div>
     </div>
   </div>
@@ -19,3 +21,35 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+</style>
